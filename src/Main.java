314ed3e4d2514006bac6722a51de7d8048c3da65
bibinur99 <binur22@mail.ru>
2023-05-04 16:41:11 +0600

    commit to HW10 to check

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello HW10");
+        task1();
+        task2();
+        task3();
     }
+
+    public static void task1() {
+        System.out.println("Task One");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " +firstName+ " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+        System.out.println();
+
+    }
+    public static void task2() {
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — "
+                + fullName.toUpperCase());
+        System.out.println();
+    }
+    public static void task3() {
+        String name = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника — " +
+                name.replace('ё','е'));
+        System.out.println();
+
+    }
+
+
 }
